ebf0adf0f781ca6967f5724b1466afa0664dabd6
starttingtoday <personalizedkim@gmail.com>
2025-03-24 00:44:02 -0700

    Update foundation_app.py

--- foundation_app.py
+++ foundation_app.py
@@ -661,7 +661,7 @@ with tab9:
                     tag_options = ["Student Design", "Peer Reviewed", "Green Foundation"]
                     selected_tags = st.multiselect("🏷️ Add Tags", tag_options, key=f"tag_{f['id']}")
                     if selected_tags:
-                        st.session_state["tags"][f['id']] = list(set(st.session_state["tags"][f['id']] + selected_tags))
+                        st.session_state["tags"][f['id']] = selected_tags
                     
                     if st.session_state["tags"][f['id']]:
                         st.info("Tags: " + ", ".join(st.session_state["tags"][f['id']]))
